ea20a44b472e9d1ffeb1dc817e72b664d0970fbf
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-03 23:29:38 +0900

    BackgroundTasks 사용해서 비동기로 실행

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, BackgroundTasks
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
+async def root():
+    return "SUCCESS"
+
+
+def save_to_file(name: str, city=""):
+    with open("text_db.txt", mode="w") as text_db:
+        row = f"{name}, {city}"
+        text_db.write(row)
+
+
+@app.post("/users")
+async def run_async(body: dict, background_tasks: BackgroundTasks):
+    background_tasks.add_task(save_to_file, body["name"], body["city"])
     return "SUCCESS"
