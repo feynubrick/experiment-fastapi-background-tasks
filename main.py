696f21b11b6ee27ad401eb702406b080e2f67ff5
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-04 19:48:56 +0900

    프린트문을 추가해서 어떤 순서로 실행되는지 확인

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, BackgroundTasks
 
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
 @app.get("/")
@@ -9,13 +9,17 @@ async def root():
     return "SUCCESS"
 
 
-def save_to_file(name: str, city=""):
-    with open("text_db.txt", mode="w") as text_db:
-        row = f"{name}, {city}"
-        text_db.write(row)
+def log_to_a_file(name: str, city=""):
+    print("log_to_a_file function called...")
+    with open("users_log.txt", mode="a") as log_file:
+        row = f"{name}, {city}\n"
+        log_file.write(row)
 
 
 @app.post("/users")
 async def run_async(body: dict, background_tasks: BackgroundTasks):
-    background_tasks.add_task(save_to_file, body["name"], body["city"])
+    print("api called!")
+    print("adding task to background tasks...")
+    background_tasks.add_task(log_to_a_file, body["name"], body["city"])
+    print("returning response...")
     return "SUCCESS"
