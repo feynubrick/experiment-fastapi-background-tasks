cbf13559f2ed2be7761ddb0a58461a307071e85e
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-06 23:47:59 +0900

    asyncio를 사용해 비동기로 처리하기

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, BackgroundTasks
+import asyncio
 
 
 app = FastAPI(debug=True)
@@ -9,7 +10,7 @@ async def root():
     return "SUCCESS"
 
 
-def log_to_a_file(name: str, city=""):
+async def log_to_a_file(name: str, city=""):
     print("log_to_a_file function called...")
     with open("users_log.txt", mode="a") as log_file:
         row = f"{name}, {city}\n"
@@ -21,5 +22,14 @@ async def run_async(body: dict, background_tasks: BackgroundTasks):
     print("api called!")
     print("adding task to background tasks...")
     background_tasks.add_task(log_to_a_file, body["name"], body["city"])
+    print("returning response...")
+    return "SUCCESS"
+
+
+@app.post("/asyncio-ensure-future")
+async def asyncio_ensure_future(body: dict):
+    print("api called!")
+    print("adding task to background tasks...")
+    asyncio.ensure_future(log_to_a_file(body["name"], body["city"]))
     print("returning response...")
     return "SUCCESS"
